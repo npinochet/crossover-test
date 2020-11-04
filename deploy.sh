f8df584400c57e004df9f8e0f0293e439036a9ce
npinochet <nnpinochet@uc.cl>
2020-11-04 01:05:40 -0300

    Better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #/bin/bash
+set -e
 
 # Clone repo
 git clone https://github.com/npinochet/crossover-test.git
+cp env crossover/backend/.env
 
 # Build and deploy frontend on S3
 cd crossover-test/frontend
-npm install
+npm ci
 npm run build-prod
 aws s3 cp dist/crossover-test-frontend/ s3://crossover-test-bucket/
 
 # Run backend
 cd ../backend
-npm install
+npm ci
 npm install forever -g
 NODE_ENV=production forever -l node.log -o out.log -e err.log start index.js
--- frontend/src/app/services/upload.service.spec.ts
+++ frontend/src/app/services/upload.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
-import { UploadService } from './upload.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UploadService } from './upload.service'
 
 describe('UploadService', () => {
   let service: UploadService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ HttpClient ]
+    });
     service = TestBed.inject(UploadService);
   });
 
