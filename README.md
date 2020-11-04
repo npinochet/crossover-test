fd6c4e0ef3763f4fde87f6eca84ff0318c27a7a0
npinochet <nnpinochet@uc.cl>
2020-11-04 00:47:43 -0300

    CI

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Crossover Full Stack Entry Test
 ![CI](https://github.com/npinochet/crossover-test/workflows/CI/badge.svg)
-![nycrc config on GitHub](https://img.shields.io/nycrc/nnpinochet/crossover-test?config=backend/.nycrc&label=coverage&preferredThreshold=80%25&style=plastic)
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'crossover-test-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('crossover-test-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('crossover-test-frontend app is running!');
-  });
 });
